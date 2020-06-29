3b64588608ede5b897ebc58c3bfb55310ff51f61
percyashu <percyayukashu@gmail.com>
2020-06-29 08:15:49 +0100

    Implemented conferencing

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+Welcome to AlcomPrototype
+Installation
+Clone the AlcomPrototype
+Start Android Studio.
+In the Quick Start panel, click Open an existing Android Studio Project.
+Navigate to the android folder, select the AlcomPrototype folder, and click Choose.
+Run AlcomPrototype on two andriod phones to test conferencing.
---
+++ app/src/main/java/com/ashu/alcomprototype/config/OpenTokConfig.java
@@ -0,0 +1,9 @@
+package com.ashu.alcomprototype.config;
+
+public class OpenTokConfig {
+
+    public static final String SESSION_ID = "2_MX40NjgxNTg0NH5-MTU5MzM2NjcwMDU1NH5EMjBxRE1wamZ2VWlFMFJzd3VsOWpVenN-fg";
+    public static final String TOKEN = "T1==cGFydG5lcl9pZD00NjgxNTg0NCZzaWc9ZjdhNzkzMTQzZmVhY2Q1ODllZTE2MjVhYzIzMWE0MTU4ZTkzNTU5NjpzZXNzaW9uX2lkPTJfTVg0ME5qZ3hOVGcwTkg1LU1UVTVNek0yTmpjd01EVTFOSDVFTWpCeFJFMXdhbVoyVldsRk1GSnpkM1ZzT1dwVmVuTi1mZyZjcmVhdGVfdGltZT0xNTkzMzY2NzYyJm5vbmNlPTAuMjQxNzg2NDI5MTM2MzE0MzImcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTU5NTk1ODc2MSZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PQ==";
+    public static final String API_KEY = "46815844";
+
+}
